bc46b945d79cfdd8fe0df9c0278139957ed608c9
roxlu <info@roxlu.com>
2013-01-21 22:36:32 +0100

    removed

--- scripts/linux/build/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/home/roxlu/roxlu_libs/scripts/linux/libxml/../build/lib"
-XML2_LIBS="-lxml2 -lz   -lm "
-XML2_INCLUDEDIR="-I/home/roxlu/roxlu_libs/scripts/linux/libxml/../build/include/libxml2"
-MODULE_VERSION="xml2-2.8.0"
-
